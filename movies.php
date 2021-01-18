c2d8636f54024eb950b86461a2c9694b778f7415
PatriziaCrimi <crimi.patrizia@gmail.com>
2021-01-18 15:36:14 +0100

    Re-organization of code: file for class "Movie" created

--- movies.php
+++ movies.php
@@ -1,57 +1,7 @@
 <?php
-  /* ASSIGNMENT
-    è definita una classe ‘Movie’
-      => all'interno della classe sono dichiarate delle variabili d'istanza
-      => all'interno della classe è definito un costruttore
-      => all'interno della classe è definito almeno un metodo
-    vengono istanziati almeno due oggetti ‘Movie’
-    e stampati a schermo i valori delle relative proprietà
-  */
-
   // VARIABLES & CONSTANTS INITIALIZATION
   $current_year = date('Y');
 
-  // CLASS CREATION
-  class Movie {
-    // INSTANCE VARIABLES INTIALIZATION
-    public $original_title;
-    public $italian_title;
-    public $original_language;
-    public $director;
-    public $lead_actor;
-    public $film_composer;
-    public $film_distributor;
-    public $release_year;
-    public $running_time;
-    public $vote;
-    public $plot;
-    public $new_release;
-
-
-    // ------------------------------ CONSTRUCTOR ------------------------------
-
-    function __construct($_original_title, $_director, $_release_year) {
-      $this->original_title = $_original_title;
-      $this->director = $_director;
-      $this->release_year = $_release_year;
-    }
-
-    // ------------------------------ METHODS ------------------------------
-
-    // GETTER FUNCTION
-    public function getRelease() {
-      return $this->release_year;
-    }
-
-    // SETTER FUNCTION
-    public function setNewRelease($release_year, $this_year) {
-      if($release_year >= $this_year) {
-        $this->new_release = true;
-      } else {
-        $this->new_release = false;
-      }
-    }
-  }
   // INSTANCES (OR OBJECTS) CREATION
   $inception = new Movie('Inception', 'Christopher Nolan', 2010);
     $inception->italian_title = 'Inception';
